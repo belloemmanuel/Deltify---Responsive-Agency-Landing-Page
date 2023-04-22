97d7a44bdad7e97038f64a790762fde79f7f9b75
Bello emmanuel <belloemmanuel02@gmail.com>
2023-04-22 18:59:04 -0500

    responsive nav

--- deltify.js
+++ deltify.js
@@ -1,5 +1,7 @@
 let hamburger = document.getElementsByClassName('menu');
 let sidebarmenu = document.getElementsByClassName('sidebar-wrapper');
+let close = document.getElementsByClassName('sidebar-close');
+
 for (i = 0; i < sidebarmenu.length; i++) {
   var sidebar = sidebarmenu[i];
 }
@@ -7,9 +9,19 @@ for (i = 0; i < hamburger.length; i++) {
   var hamburgerImg = hamburger[i];
 }
 
+for (i = 0; i < close.length; i++) {
+  var closeBtn = close[i];
+}
+
 hamburgerImg.addEventListener('click', open);
+closeBtn.addEventListener('click', closenav);
 
 function open(e) {
   sidebar.style.display = 'block';
+  e.preventDefault();
+}
+
+function closenav(e) {
+  sidebar.style.display = 'none';
   e.preventDefault();
 }
