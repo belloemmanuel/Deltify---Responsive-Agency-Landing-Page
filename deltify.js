57ebfa9df173baf3b7fc1bcbf2128e92e6ac77b2
Bello emmanuel <belloemmanuel02@gmail.com>
2023-04-25 09:03:20 -0500

    js updated

--- deltify.js
+++ deltify.js
@@ -1,25 +1,28 @@
-let hamburger = document.getElementsByClassName('menu');
-let sidebarmenu = document.getElementsByClassName('sidebar-wrapper');
-let close = document.getElementsByClassName('sidebar-close');
+// let hamburger = document.getElementsByClassName('menu');
+// let sidebarmenu = document.getElementsByClassName('sidebar-wrapper');
+// let close = document.getElementsByClassName('sidebar-close');
+const menuId = document.getElementById('menuid');
+const sidebarWrapper = document.getElementById('sidebar-wrapper');
+const sidebarClose = document.getElementById('sidebar-close');
 
-for (i = 0; i < sidebarmenu.length; i++) {
-  var sidebar = sidebarmenu[i];
-}
-for (i = 0; i < hamburger.length; i++) {
-  var hamburgerImg = hamburger[i];
-}
+// for (i = 0; i < sidebarmenu.length; i++) {
+//   var sidebar = sidebarmenu[i];
+// }
+// for (i = 0; i < hamburger.length; i++) {
+//   var hamburgerImg = hamburger[i];
+// }
 
-for (i = 0; i < close.length; i++) {
-  var closeBtn = close[i];
-}
+// for (i = 0; i < close.length; i++) {
+//   var closeBtn = close[i];
+// }
 
-hamburgerImg.addEventListener('click', open);
-closeBtn.addEventListener('click', closenav);
+menuId.addEventListener('click', open);
+sidebarClose.addEventListener('click', closenav);
 
 function open() {
-  sidebar.style.display = 'block';
+  sidebarWrapper.style.display = 'block';
 }
 
 function closenav() {
-  sidebar.style.display = 'none';
+  sidebarWrapper.style.display = 'none';
 }
